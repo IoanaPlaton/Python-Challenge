a4e63c487c97ca70bbdb64ecafd36f6b8894106b
IoanaPlaton <platon.ioana@gmail.com>
2024-07-01 20:11:12 -0600

    Updating Python File

--- PyPoll/PyPollMain.py
+++ PyPoll/PyPollMain.py
@@ -1,161 +1,114 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "ffe2d582-c6e5-4e7f-8c30-99c76a1e02ab",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 114,
-   "id": "5245ac77-1d19-404e-965a-da82ed9215e5",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Election Results\n",
-      "--------------------------\n",
-      "Total Votes: 369711\n",
-      "--------------------------\n",
-      "Charles Casper Stockham: 23% (85213)\n",
-      "Diana DeGette: 74% (272892)\n",
-      "Raymon Anthony Doane: 3% (11606)\n",
-      "--------------------------\n",
-      "Winner: Diana DeGette\n"
-     ]
-    }
-   ],
-   "source": [
-    "#PyPoll Instructions\n",
-    "\n",
-    "#In this Challenge, you are tasked with helping a small, rural town modernize its vote-counting process.\n",
-    "#You will be given a set of poll data called election_data.csv. The dataset is composed of three columns: “Voter ID”, “County”, and “Candidate”. Your task is to create a Python script that analyzes the votes and calculates each of the following values:\n",
-    "\n",
-    "#The total number of votes cast\n",
-    "#A complete list of candidates who received votes\n",
-    "#The percentage of votes each candidate won\n",
-    "#The total number of votes each candidate won\n",
-    "#The winner of the election based on popular vote\n",
-    "\n",
-    "import os\n",
-    "import csv\n",
-    "\n",
-    "election_data = os.path.join ('.' , 'Resources' , 'election_data.csv')\n",
-    "\n",
-    "#Candidate Options and Vote Counters \n",
-    "candidates = []\n",
-    "vote_count = []\n",
-    "percent_votes = []\n",
-    "\n",
-    "# Total vote counter\n",
-    "total_votes = 0\n",
-    "\n",
-    "#Winning Candidate and Winning County Tracker\n",
-    "\n",
-    "#open and read csv \n",
-    "with open (election_data, newline = \"\") as csvfile:\n",
-    "    csv_reader = csv.reader(csvfile, delimiter=\",\")\n",
-    "    #print(csv_reader)\n",
-    "\n",
-    "# Read the header row first \n",
-    "    csv_header = next(csv_reader)\n",
-    "    #print(csv_header)\n",
-    "\n",
-    "#Add candidates and their votes to the list. If candidte is on the list only add to their vote count.    \n",
-    "    for row in csv_reader:\n",
-    "        total_votes += 1\n",
-    "        \n",
-    "        if row[2] not in candidates:\n",
-    "            candidates.append(row[2])\n",
-    "            index = candidates.index(row[2])\n",
-    "            vote_count.append(1)\n",
-    "        else:\n",
-    "            index = candidates.index(row[2])\n",
-    "            vote_count[index] += 1\n",
-    "            \n",
-    "#The total number of votes cast\n",
-    "print(\"Election Results\")\n",
-    "print(\"--------------------------\")\n",
-    "print(f\"Total Votes: {str(total_votes)}\")\n",
-    "print(\"--------------------------\")\n",
-    "\n",
-    "#A complete list of candidates who received votes & #The total number of votes each candidate won      \n",
-    "#The percentage of votes each candidate won\n",
-    "\n",
-    "for votes in vote_count:\n",
-    "    percentage = (votes/total_votes) * 100\n",
-    "    percentage = round(percentage)\n",
-    "    percentage = \"%.0f%%\" % percentage\n",
-    "    percent_votes.append(percentage)\n",
-    "\n",
-    "for i in range(len(candidates)):\n",
-    "    print(f\"{candidates[i]}: {str(percent_votes[i])} ({str(vote_count[i])})\")\n",
-    "\n",
-    "#The winner of the election based on popular vote\n",
-    "winner = max(vote_count)\n",
-    "index = vote_count.index(winner)\n",
-    "winning_candidate = candidates[index]\n",
-    "\n",
-    "print(\"--------------------------\")\n",
-    "\n",
-    "print(f\"Winner: {winning_candidate}\")\n",
-    "\n",
-    "#In addition, your final script should both print the analysis to the terminal and export a text file with the results.\n",
-    "\n",
-    "output_path = os.path.join('.', 'Analysis', 'PollAnalysis.txt')   \n",
-    "with open(output_path, \"w\") as txtfile:\n",
-    "    txtfile.write(\"Election Results\" + \"\\n\")\n",
-    "\n",
-    "    txtfile.write(\"--------------------------\" + \"\\n\")\n",
-    "\n",
-    "    txtfile.write(f\"Total Votes: {str(total_votes)}\" + \"\\n\")\n",
-    "\n",
-    "    txtfile.write(\"--------------------------\" + \"\\n\")\n",
-    "    \n",
-    "    for i in range(len(candidates)):\n",
-    "        line = str(f\"{candidates[i]}: {str(percent_votes[i])} ({str(vote_count[i])})\")\n",
-    "        txtfile.write('{}\\n'.format(line))\n",
-    "    \n",
-    "    txtfile.write(\"--------------------------\" + \"\\n\")\n",
-    "\n",
-    "    txtfile.write(f\"Winner: {winning_candidate}\" + \"\\n\")\n",
-    "\n",
-    "\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "8d834181-748b-4266-b227-fb2a1def3565",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[114]:
+
+
+#PyPoll Instructions
+
+#In this Challenge, you are tasked with helping a small, rural town modernize its vote-counting process.
+#You will be given a set of poll data called election_data.csv. The dataset is composed of three columns: “Voter ID”, “County”, and “Candidate”. Your task is to create a Python script that analyzes the votes and calculates each of the following values:
+
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote
+
+import os
+import csv
+
+election_data = os.path.join ('.' , 'Resources' , 'election_data.csv')
+
+#Candidate Options and Vote Counters 
+candidates = []
+vote_count = []
+percent_votes = []
+
+# Total vote counter
+total_votes = 0
+
+#Winning Candidate and Winning County Tracker
+
+#open and read csv 
+with open (election_data, newline = "") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    #print(csv_reader)
+
+# Read the header row first 
+    csv_header = next(csv_reader)
+    #print(csv_header)
+
+#Add candidates and their votes to the list. If candidte is on the list only add to their vote count.    
+    for row in csv_reader:
+        total_votes += 1
+        
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            index = candidates.index(row[2])
+            vote_count.append(1)
+        else:
+            index = candidates.index(row[2])
+            vote_count[index] += 1
+            
+#The total number of votes cast
+print("Election Results")
+print("--------------------------")
+print(f"Total Votes: {str(total_votes)}")
+print("--------------------------")
+
+#A complete list of candidates who received votes & #The total number of votes each candidate won      
+#The percentage of votes each candidate won
+
+for votes in vote_count:
+    percentage = (votes/total_votes) * 100
+    percentage = round(percentage)
+    percentage = "%.0f%%" % percentage
+    percent_votes.append(percentage)
+
+for i in range(len(candidates)):
+    print(f"{candidates[i]}: {str(percent_votes[i])} ({str(vote_count[i])})")
+
+#The winner of the election based on popular vote
+winner = max(vote_count)
+index = vote_count.index(winner)
+winning_candidate = candidates[index]
+
+print("--------------------------")
+
+print(f"Winner: {winning_candidate}")
+
+#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+output_path = os.path.join('.', 'Analysis', 'PollAnalysis.txt')   
+with open(output_path, "w") as txtfile:
+    txtfile.write("Election Results" + "\n")
+
+    txtfile.write("--------------------------" + "\n")
+
+    txtfile.write(f"Total Votes: {str(total_votes)}" + "\n")
+
+    txtfile.write("--------------------------" + "\n")
+    
+    for i in range(len(candidates)):
+        line = str(f"{candidates[i]}: {str(percent_votes[i])} ({str(vote_count[i])})")
+        txtfile.write('{}\n'.format(line))
+    
+    txtfile.write("--------------------------" + "\n")
+
+    txtfile.write(f"Winner: {winning_candidate}" + "\n")
+
+
+
+
+# In[ ]:
+
+
+
+
